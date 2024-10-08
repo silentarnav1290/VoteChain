5a48f3ab215277034782ae9ce29c8d54dc644e4a
Arnav Chopra <68425420+silentarnav1290@users.noreply.github.com>
2024-10-09 00:16:26 +0530

    simplestorage1

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/";
 
-contract SimpleCollectible is ERC721 {
-    
+contract SimpleCollectible is ERC721URIStorage {
+    uint256 public tokenCounter;
+    constructor () public ERC721 ("Dogie", "DOG"){
+        tokenCounter = 0;
+    }
+
+    function createCollectible(string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newItemId;
+    }
 }
